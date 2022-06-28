31974bdd83880084188de52c40e70fe10f7d6b46
PChol22 <pchol.pro@gmail.com>
2022-06-28 23:24:14 +0200

    feat(Design): create first color palette

---
+++ src/Theme.ts
@@ -0,0 +1,31 @@
+export const Theme = {
+  palette: {
+    principal: {
+      light: '#76AAFF',
+      main: '#0B67C2',
+      dark: '#001C65',
+    },
+    secondary: {
+      light: '#A6D5FF',
+      main: '#0091DC',
+      dark: '#4758A3',
+    },
+    oppositeA: {
+      light: '#F56E89',
+      main: '#C52155',
+      dark: '#9B0736',
+    },
+    oppositeB: {
+      light: '#D8ECEA',
+      main: '#2AC195',
+      dark: '#008A62',
+    },
+    grey: {
+      white: '#F5F9FF',
+      light: '#979EB8',
+      main: '#5C6680',
+      dark: '#3E4561',
+      black: '#010D2B',
+    },
+  },
+};
