79fdef207ae5bdbf794f5fa12929954f9041308d
PChol22 <pchol.pro@gmail.com>
2022-07-05 00:54:15 +0200

    feat(Landing Page): create main content on the right

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { Box, Stack, Typography } from "@mui/material";
 import { AppTheme, PAGE_PADDING_X } from "./AppTheme";
 import { Button } from "./Button";
+import { MainStructure } from "./MainStructure";
+import Portrait from './Portrait.png';
 
 export const LandingPage = (): JSX.Element =>
-  <Stack height="100vh" paddingX={PAGE_PADDING_X} justifyContent="center">
+  <Stack height="100vh" paddingX={PAGE_PADDING_X} justifyContent="center" position="relative" overflow="hidden">
     <Stack flexDirection="row">
       <Stack gap={1}>
         <Typography fontSize={80} fontWeight={600} lineHeight={1} color={AppTheme.palette.grey.dark}>
@@ -20,4 +22,8 @@ export const LandingPage = (): JSX.Element =>
         <Button fontSize={18} colorVariant="secondary" styleVariant="filled" width="max-content">Reach me !</Button>
       </Stack>
     </Stack>
-  </Stack>;
+    <Box position="absolute" right={0} width={400} marginRight="10vw">
+      <img src={Portrait} width="100%" />
+    </Box>
+    <MainStructure />
+  </Stack >;
