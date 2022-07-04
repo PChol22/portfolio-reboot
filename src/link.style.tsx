2504a1a4ea2912922c00afe7e7d667a3a50c74e6
PChol22 <pchol.pro@gmail.com>
2022-07-04 23:28:49 +0200

    feat(Theme): create clickable styled link

---
+++ src/link.style.tsx
@@ -0,0 +1,29 @@
+import { Box, styled } from "@mui/material";
+import { AppTheme } from "./AppTheme";
+
+export const StyledLink = styled(Box)<{ colorVariant: 'principal' | 'secondary' | 'oppositeA' | 'oppositeB' }>(({ colorVariant }) => ({
+  cursor: 'pointer',
+  color: AppTheme.palette[colorVariant].main,
+  position: 'relative',
+  '&::after': {
+    background: AppTheme.palette[colorVariant].main,
+    position: 'absolute',
+    width: "100%",
+    height: '1.5px',
+    bottom: -1,
+    left: 0,
+    content: "''",
+    transformOrigin: 'center',
+    transform: 'scaleX(0)'
+  },
+  '&:hover::after': {
+    transform: 'scaleX(0.8)',
+    transition: "transform 400ms"
+  },
+  '&:active': {
+    color: AppTheme.palette[colorVariant].dark,
+  },
+  '&:active::after': {
+    background: AppTheme.palette[colorVariant].dark,
+  },
+}));
