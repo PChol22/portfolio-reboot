dd29290e559141852c24d276c91d25278c7898dd
PChol22 <pchol.pro@gmail.com>
2022-07-12 23:24:22 +0200

    feat(Middle Layer): create middle layer and experience components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from './Header';
 import { LandingPage } from './LandingPage';
+import { MiddleLayer } from './MiddleLayer';
 
 export const App = (): JSX.Element => (
   <>
     <LandingPage></LandingPage>
     <Header></Header>
+    <MiddleLayer></MiddleLayer>
   </>
 );
---
+++ src/MiddleLayer.tsx
@@ -0,0 +1,20 @@
+import { Box, Stack, styled, Typography } from "@mui/material";
+import { AppTheme, PAGE_PADDING_X } from "./AppTheme";
+
+const MiddleLayerContainer = styled(Box)(() => ({
+  background: AppTheme.palette.grey.light,
+  width: '100vw',
+  borderBottom: '1px solid',
+  borderColor: AppTheme.palette.grey.main,
+}));
+
+export const MiddleLayer = (): JSX.Element => (
+  <Box height="40vh">
+    <MiddleLayerContainer paddingX={PAGE_PADDING_X} paddingY={2}>
+      <Stack alignItems="flex-start">
+        <Typography fontSize={72} color={AppTheme.palette.secondary.main} fontWeight={600} lineHeight={1}>15+</Typography>
+        <Typography fontSize={24} color={AppTheme.palette.grey.dark} lineHeight={1} fontWeight={600} marginTop={0.5}>Months<br />Professional<br />Experience</Typography>
+      </Stack>
+    </MiddleLayerContainer>
+  </Box>
+);
